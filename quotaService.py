48a44f67b17aa343b314c641f455ed25605ac89e
Thomas Winther <47111266+messimaster3@users.noreply.github.com>
2021-12-07 11:12:03 +0100

    Setup files, and bas function

---
+++ quotaService.py
@@ -0,0 +1,30 @@
+
+
+
+# This function takes
+def GetQuotaRequest(user_id):
+     memory: float
+     vCpu: int
+
+
+     convertToJson()
+     return
+
+def UpdateMemoryQuotaRequest(user_id, memory):
+    user_id: str
+    memory: int
+
+def UpdateVCpuQuotaRequest(user_id, vcpu):
+    user_id: str
+    vcpu: int
+
+
+
+def writeToDB(object):
+    return
+
+def readFromDB():
+     return
+
+def convertToJson(object):
+    return  
