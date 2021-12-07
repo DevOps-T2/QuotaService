ea85290f330317bda4961e152da27d33f06000e7
Thomas Winther <47111266+messimaster3@users.noreply.github.com>
2021-12-07 19:26:32 +0100

    add GetQuotaRequest and readDB,  + some exection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import AsyncIterable
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import mysql.connector
 from mysql.connector import Error
@@ -9,35 +9,47 @@ app = FastAPI()
 class UpdateMemory(BaseModel):
     memoryValue: float
 
-class Vcpu(BaseModel):
+class UpdateVcpu(BaseModel):
     vcpuValue: int
 
 
 # This function takes a userID and returns the memory and Vcpu limits for ths user
 @app.get("/quota/{user_id}")
-def GetQuotaRequest(user_id):
-     memory: float
-     vCpu: int
+async def GetQuotaRequest(user_id: str):
+    memory: float
+    vCpu: int
 
-     return
+    memory, vCpu = readFromDB(user_id)
 
+    if memory or vCpu == -1:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    return memory, vCpu
+    
 # Updates the memory limit for a specific user
 @app.put("/quota/memory/{user_id}")
 async def UpdateMemoryQuotaRequest(user_id: str, updateMemory: UpdateMemory):
 
     # Does a mysql database return a status code, when a write is succes???
-    writeToDB(user_id, "upMemory", updateMemory.memoryValue)
+    statusCode = writeToDB(user_id, "upMemory", updateMemory.memoryValue)
+
+    if statusCode == 404:
+        raise HTTPException(status_code=404, detail="User not found, Memory limit NOT updated")
     
-    return 
+    return "User memory was updated" 
 
 # Updates the Vcpu limit for a specific user
 @app.put("/quota/Vcpu/{user_id}")
-async def UpdateVCpuQuotaRequest(user_id: str, vcpu: Vcpu):
+async def UpdateVCpuQuotaRequest(user_id: str, vcpu: UpdateVcpu):
     
-    writeToDB(user_id, "upVcpu", vcpu.vcpuValue )
+    statusCode = writeToDB(user_id, "upVcpu", vcpu.vcpuValue )
+    
+    if statusCode == 404:
+        raise HTTPException(status_code=404, detail="User not found, Vcpu limit NOT updated")
 
-    return
-
+    return "User Vcpu updated"
+    
+# This function is a event and NOT a endpoint
 def CreatQuota(user_id, vcpu, memory):
     return
 
@@ -61,10 +73,19 @@ def writeToDB(user_id: str, operation: str, item):
                     mycursor.execute(mysqlquery)
 
                     connection.commit()
-        
+
                     print("Memory for user " + user_id + " updated")
+
+                    statusCode = 200
+    
+                    return statusCode  
+        
+                    
                 else:
                     print("No user found")
+                    statusCode = 404
+ 
+                    return statusCode
         
             elif operation == "upVcpu":
                 # Check to see if the userID exits in the database
@@ -79,9 +100,17 @@ def writeToDB(user_id: str, operation: str, item):
                     connection.commit()
         
                     print("CPU for user " + user_id + " updated")
+
+                    statusCode = 200
+                
+                    return statusCode 
             
                 else:
                     print("No user found")
+                    
+                    statusCode = 404
+
+                    return statusCode
               
     except Error as e:
         print("Error while connecting to MySQL", e)
@@ -92,5 +121,46 @@ def writeToDB(user_id: str, operation: str, item):
             connection.close()
             print("MySQL connection is closed")
 
-def readFromDB():
-     return
+def readFromDB(user_id):
+    try:
+        connection = mysql.connector.connect(host='localhost',
+                                         database='mydatabase',
+                                         user='root',
+                                         password='1234')
+      
+        if connection.is_connected():
+            mycursor = connection.cursor()
+            # Check to see if the userID exits in the database
+            sqlquery = "SELECT COUNT(*) FROM quotastabel WHERE User_id =" + "\"" +  user_id + "\""
+            mycursor.execute(sqlquery)
+            resultFromquery = mycursor.fetchall()[0][0]
+
+            if resultFromquery == 1:
+                mysqlquery = "SELECT Vcpu, Memory From quotastabel WHERE User_id = " + "\"" +  user_id + "\""
+                mycursor.execute(mysqlquery)
+
+                resultFromquery = mycursor.fetchall()
+                print("succes user_id found")
+
+                valueCPU = resultFromquery[0][0]
+                valueMemory = resultFromquery[0][1]
+
+                print("For user " + user_id + " the number of vcpus is " + str(valueCPU))
+                print("For user " + user_id + " the number of Memory is " + str(valueMemory))
+
+                return valueMemory, valueCPU
+
+            else:
+                valueCPU = -1
+                valueMemory = -1
+
+                return valueMemory, valueCPU
+              
+    except Error as e:
+        print("Error while connecting to MySQL", e)
+
+    finally:
+        if connection.is_connected():
+            mycursor.close()
+            connection.close()
+            print("MySQL connection is closed")
