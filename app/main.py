4e72d35b417bdd818056f5a105be368dc096816e
Thomas Winther <47111266+messimaster3@users.noreply.github.com>
2021-12-08 16:46:45 +0100

    test

--- app/main.py
+++ app/main.py
@@ -180,3 +180,33 @@ def readFromDB(user_id):
             mycursor.close()
             connection.close()
             print("MySQL connection is closed")
+
+
+@app.get("/")
+def read_root():
+
+    test = ''    
+    try:
+        connection = mysql.connector.connect(host=ownHostForWrite,
+                                         database=ownDatabase,
+                                         user=ownUser
+                                         )
+        if connection.is_connected():
+            db_Info = connection.get_server_info()
+            print("Connected to MySQL Server version ", db_Info)
+            cursor = connection.cursor()
+            cursor.execute("select database();")
+            record = cursor.fetchone()
+            print("You're connected to database: ", record)
+            
+            return "succes!!!"
+
+    except Error as e:
+        print("Error while connecting to MySQL", e)
+    finally:
+        if connection.is_connected():
+           cursor.close()
+           connection.close()
+           print("MySQL connection is closed")
+
+    return "failll"
